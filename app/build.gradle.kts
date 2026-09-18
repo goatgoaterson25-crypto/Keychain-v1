abf53d5dfb3ab714d4a0c437f9573e1dc8f7b085
goatgoaterson25-crypto <goatgoaterson25@gmail.com>
2026-09-18 15:18:48 -0500

    🥺 Injected Build Configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.apollo.Keychain-v1"
+    namespace = "com.apollo.keychainv1"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.apollo.Keychain-v1"
+        applicationId = "com.apollo.keychainv1"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
